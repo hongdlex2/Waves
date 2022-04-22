40829e5150c6cd26a23ebaf49c18b840e431f309
hongdlex2 <ghdals1997@naver.com>
2022-04-22 11:52:26 +0900

    waveGroup

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import{
-    Wave
-}from './wave.js';
+    WaveGroup
+}from './waveGroup.js';
 class App{
     constructor(){
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
-        this.wave = new Wave();
+        this.waveGroup = new WaveGroup();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -23,12 +23,12 @@ class App{
         this.canvas.height = this.stageHeight *2;
         this.ctx.scale(2,2);
 
-        this.wave.resize(this.stageWidth, this.stageHeight);
+        this.waveGroup.resize(this.stageWidth, this.stageHeight);
     }
 
     animate(t){
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-        this.wave.draw(this.ctx);
+        this.waveGroup.draw(this.ctx);
         window.requestAnimationFrame(this.animate.bind(this));
     }
 }
