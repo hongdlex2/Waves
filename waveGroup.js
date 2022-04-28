c0942cd47a47974f291829c5d33c581e2afcffdf
hongdlex2 <ghdals1997@naver.com>
2022-04-28 11:39:08 +0900

    waves done

--- waveGroup.js
+++ waveGroup.js
@@ -4,14 +4,14 @@ import{
 
 export class WaveGroup{
     constructor(){
-        this.totalWaves = 1;
+        this.totalWaves = 3;
         this.totalPoints = 6;
 
-        this.color = ['rgba(0, 199, 235, 0.4)', 'rgba(0, 146, 199, 0.4)', 'rgba(0, 87, 158, 0.4)'];
+        this.color = ['rgba(255, 0, 0, 0.4)', 'rgba(255, 255, 0, 0.4)', 'rgba(0, 255, 255, 0.4)'];
 
         this.waves = [];
 
-        for(let i = 0; i < this.tatalWaves; i++){
+        for(let i = 0; i < this.totalWaves; i++){
             const wave = new Wave(
                 i,
                 this.totalPoints,
@@ -22,7 +22,7 @@ export class WaveGroup{
     }
 
     resize(stageWidth, stageHeight){
-        for(let i = 0; i < this.tatalWaves; i++){
+        for(let i = 0; i < this.totalWaves; i++){
             const wave = this.waves[i];
             wave.resize(stageWidth, stageHeight);
         }
